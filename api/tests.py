046171be31e2d257384caafcf162beb0382d1491
Arnold <arnoldnderitu@gmail.com>
2023-05-17 11:02:56 +0300

    add tests module

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class HelloWorldAPITest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_hello_world(self):
+        response = self.client.get("/api/hello-world/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {"message": "Hello world"})
